c0ff2365b7e815597e83c284e5542d6e018945ea
Hiroki Kondo <kompiro@gmail.com>
2015-07-07 09:31:43 +0900

    Enable logging
    - Use @Slf4j
    - Add logback-test.groovy

--- src/main/groovy/org/kompiro/EclipsePluginDeployer.groovy
+++ src/main/groovy/org/kompiro/EclipsePluginDeployer.groovy
@@ -1,18 +1,32 @@
 package org.kompiro
+
+import groovy.util.logging.Slf4j
+
 /**
  * Created by kompiro on 2015/07/06.
  */
+@Slf4j
 class EclipsePluginDeployer {
     static String ECLIPSE_BASE = '/Applications/Eclipse.app/Contents/Eclipse'
 
+    PomGenerator pomGenerator = new PomGenerator()
+    EclipsePlugins plugins
+
+    EclipsePluginDeployer(String base){
+        log.info("target eclipse : {}",base)
+        this.plugins = new EclipsePlugins(base)
+    }
+
+    def deploy(String pluginName) {
+        log.info "start Eclipse Plugin Deployer"
+        def plugin = plugins.get(pluginName)
+        def binding = plugin.binding()
+        def generated = pomGenerator.generate binding
+        log.info generated
+    }
+
     static def main(String[] args) {
-        def plugins = new EclipsePlugins(ECLIPSE_BASE);
-
-        println "start Eclipse Plugin Deployer"
-        println "target eclipse: ${ECLIPSE_BASE}"
-
-        def plugin = plugins.get("org.eclipse.core.runtime")
-        def generator = new PomGenerator()
-        println generator.generate(plugin.binding())
+        def deployer = new EclipsePluginDeployer(ECLIPSE_BASE)
+        deployer.deploy('org.eclipse.core.runtime')
     }
 }
---
+++ src/test/resources/logback-test.groovy
@@ -0,0 +1,16 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+import static ch.qos.logback.classic.Level.*
+
+final consoleAppender = 'console'
+
+scan('30 seconds')
+
+appender(consoleAppender, ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = '%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n'
+    }
+}
+
+root(INFO, [consoleAppender])
