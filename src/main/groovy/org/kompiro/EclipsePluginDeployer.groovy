8b3abc7071aa046ab8e7060ded44681ef1bbd50b
Hiroki Kondo <kompiro@gmail.com>
2015-07-07 17:15:52 +0900

    Enable to generate specified pom.xml

--- src/main/groovy/org/kompiro/EclipsePluginDeployer.groovy
+++ src/main/groovy/org/kompiro/EclipsePluginDeployer.groovy
@@ -2,31 +2,46 @@ package org.kompiro
 
 import groovy.util.logging.Slf4j
 
+import java.nio.file.Path
+import java.nio.file.Paths
+
 /**
  * Created by kompiro on 2015/07/06.
  */
 @Slf4j
 class EclipsePluginDeployer {
-    static String ECLIPSE_BASE = '/Applications/Eclipse.app/Contents/Eclipse'
+    static final String ECLIPSE_BASE = '/Applications/Eclipse.app/Contents/Eclipse'
+    static Path GENERATE_PATH = Paths.get('/tmp/','generated')
 
     PomGenerator pomGenerator = new PomGenerator()
     EclipsePlugins plugins
+    Path generatePath
 
-    EclipsePluginDeployer(String base){
-        log.info("target eclipse : {}",base)
-        this.plugins = new EclipsePlugins(base)
+    EclipsePluginDeployer(String eclipseBase, Path generatePath){
+        log.info("target eclipse : {}",eclipseBase)
+        this.plugins = new EclipsePlugins(eclipseBase)
+        this.generatePath = generatePath
     }
 
     def deploy(String pluginName) {
         log.info "start Eclipse Plugin Deployer"
+        if(!generatePath.toFile().exists()) {
+            generatePath.toFile().mkdirs()
+        }
         def plugin = plugins.get(pluginName)
+        generatePom(plugin)
+    }
+
+    def generatePom(EclipsePlugin plugin){
+        def pluginPomPath = generatePath.resolve("${plugin.name}_pom.xml")
+        log.info("Generate pom : $pluginPomPath")
         def binding = plugin.binding()
         def generated = pomGenerator.generate binding
-        log.info generated
+        pluginPomPath.write(generated)
     }
 
     static def main(String[] args) {
-        def deployer = new EclipsePluginDeployer(ECLIPSE_BASE)
+        def deployer = new EclipsePluginDeployer(ECLIPSE_BASE,GENERATE_PATH)
         deployer.deploy('org.eclipse.core.runtime')
     }
 }
